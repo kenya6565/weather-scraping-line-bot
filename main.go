b8c6940f4e24ad1ab0972a921a7a9de8f8c9a005
kenya6565 <ken.australia0605@gmail.com>
2023-08-10 02:05:07 +0900

    fix main.go to activate localhost8080

--- main.go
+++ main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
-	"fmt"
-	"weather-scraping-line-bot/weather"
+	"log"
+	"net/http"
+
+	notification "weather-scraping-line-bot/notification"
 )
 
 const YokohamaWestAreaCode = "140020"
 
-func main() {
-	weatherReport, err := weather.FetchWeatherReport()
-	if err != nil {
-		fmt.Println(err)
-		return
+func handleCallback(w http.ResponseWriter, r *http.Request) {
+	events, _ := notification.Bot.ParseRequest(r)
+	for _, event := range events {
+		notification.HandleEvent(event)
 	}
+}
 
-	areas, timeSeriesInfos := weather.FilterAreas(weatherReport, YokohamaWestAreaCode)
-	weather.ProcessAreaInfos(areas, timeSeriesInfos)
+func main() {
+	// weatherReport, err := weather.FetchWeatherReport()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// areas, timeSeriesInfos := weather.FilterAreas(weatherReport, YokohamaWestAreaCode)
+	// weather.ProcessAreaInfos(areas, timeSeriesInfos)
+
+	// execute handleCallback when receiving request /callback
+	http.HandleFunc("/callback", handleCallback)
+	log.Println("Starting server on :8080...")
+	// activate http server 
+	http.ListenAndServe(":8080", nil)
 }
