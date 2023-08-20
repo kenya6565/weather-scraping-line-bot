6557868c87844180cef67a3361c69ec02d4c337a
kenya6565 <ken.australia0605@gmail.com>
2023-08-20 23:57:05 +0900

    create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+# Cloud Functionsのリソース
+resource "google_cloudfunctions_function" "weather_notifier" {
+  name                  = "weather-notifier"
+  available_memory_mb   = 256
+  runtime               = "go113" # Goのランタイム。必要に応じてバージョンを変更
+  source_archive_bucket = google_storage_bucket.cloudfunctions_bucket.name
+  source_archive_object = google_storage_bucket_object.source_archive.name
+
+  trigger_http = true
+
+  entry_point = "WeatherNotifierFunction" # 関数のエントリーポイント。変更する場合はここを修正
+}
+
+resource "google_storage_bucket" "cloudfunctions_bucket" {
+  name     = "weather-notifier-functions-bucket"
+  location = "us-central1"
+}
+
+resource "google_storage_bucket_object" "source_archive" {
+  name   = "function-source.zip"
+  bucket = google_storage_bucket.cloudfunctions_bucket.name
+  source = "${path.module}/../function-source.zip"
+}
+
+# Cloud Schedulerのジョブ
+resource "google_cloud_scheduler_job" "daily_weather_check" {
+  name     = "daily-weather-check"
+  schedule = "0 0 * * *" # 毎日0時に実行
+
+  http_target {
+    uri                  = google_cloudfunctions_function.weather_notifier.https_trigger_url
+    http_method          = "GET"
+  }
+}
+
+# Firestoreの設定は事前に終えていたのと今後変更する事がないのでterraformに含まない
