3b493ffa471a3d8bc5f1138c763c085ad8b4cf46
kenya6565 <ken.australia0605@gmail.com>
2023-09-27 02:28:56 +0900

    create interface.go

--- app/services/weather/interface.go
+++ app/services/weather/interface.go
@@ -1,1 +1,17 @@
 package weather
+
+// WeatherProcessor defines the methods required for processing weather information for cities.
+type WeatherProcessor interface {
+	FetchDataFromJMA() (WeatherData, error)
+	Process(data WeatherData) ProcessedWeatherInfo
+}
+
+// WeatherData represents raw weather data for a city.
+type WeatherData struct {
+	// Define fields for raw weather data here.
+}
+
+// ProcessedWeatherInfo represents processed weather information for a city.
+type ProcessedWeatherInfo struct {
+	// Define fields for processed weather information here.
+}
