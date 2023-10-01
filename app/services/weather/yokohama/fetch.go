06ea7a934614f21cfb2186a280a28f14c9b7ae56
kenya6565 <ken.australia0605@gmail.com>
2023-10-02 02:32:42 +0900

    fix fetch.go

--- app/services/weather/yokohama/fetch.go
+++ app/services/weather/yokohama/fetch.go
@@ -1,22 +1,34 @@
-package weather
+package yokohama
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
+
 )
 
-const JmaApiEndpoint = "https://www.jma.go.jp/bosai/forecast/data/forecast/140000.json"
+type YokohamaWeatherProcessor struct {
+	JmaApiEndpoint string
+	AreaCode       string
+}
 
 // FetchDataFromJma makes a GET request to the JMA API, reads the response body, and returns it.
-func FetchDataFromJMA() ([]byte, error) {
-	resp, err := http.Get(JmaApiEndpoint)
+func (y *YokohamaWeatherProcessor) FetchDataFromJMA() ([]WeatherInfo, error) {
+
+	resp, err := http.Get(y.JmaApiEndpoint)
 	if err != nil {
 		return nil, err
 	}
 	// Close the response body once all operations on it are done.
 	// This is essential to release resources and avoid potential memory leaks.
 	defer resp.Body.Close()
-
 	// Read the entire response body and return its contents.
-	return io.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var weatherReport []WeatherInfo
+	err = json.Unmarshal(body, &weatherReport)
+	return weatherReport, err
 }
