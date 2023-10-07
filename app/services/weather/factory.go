dff4b7498db9f7a96cec139bdcf310bb197c496c
kenya6565 <ken.australia0605@gmail.com>
2023-10-07 22:10:12 +0900

    fix not defining structure in factory.go

--- app/services/weather/factory.go
+++ app/services/weather/factory.go
@@ -3,16 +3,15 @@ package weather
 import (
 	"errors"
 
-	"github.com/kenya6565/weather-scraping-line-bot/app/services/weather/yokohama"
+	yokohama "github.com/kenya6565/weather-scraping-line-bot/app/services/weather/yokohama"
 )
 
+// 構造体の定義はfactory.goでは行わない。
 func GetWeatherProcessorForCity(city string) (WeatherProcessor, error) {
 	switch city {
 	case "yokohama":
-		return &yokohama.YokohamaWeatherProcessor{
-			JmaApiEndpoint: "https://www.jma.go.jp/bosai/forecast/data/forecast/140000.json",
-			AreaCode:       "140020",
-		}, nil
+		// Create a new instance of YokohamaWeatherProcessor
+		return yokohama.NewYokohamaWeatherProcessor(), nil
 	// 他の都市も必要に応じて追加する
 	default:
 		return nil, errors.New("unknown city")
