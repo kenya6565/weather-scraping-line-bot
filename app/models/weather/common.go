e50893b08e1c712bc7c3214c4896829330cae474
kenya6565 <ken.australia0605@gmail.com>
2023-10-08 20:36:02 +0900

    add comment out

--- app/models/weather/common.go
+++ app/models/weather/common.go
@@ -4,6 +4,7 @@ type WeatherInfo struct {
 	TimeSeries []TimeSeriesInfo `json:"timeSeries"`
 }
 
+// Areas varies, depending on cities.
 type TimeSeriesInfo struct {
 	Areas       []AreaInfoInterface `json:"areas"`
 	TimeDefines []string            `json:"timeDefines"`
