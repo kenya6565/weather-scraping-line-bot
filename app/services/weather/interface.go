b5352f3421f4bb650266a6043cd19d2ed636dadc
kenya6565 <ken.australia0605@gmail.com>
2023-10-10 23:52:01 +0900

    remove unused struct and add another method to WeatherProcessor interface

--- app/services/weather/interface.go
+++ app/services/weather/interface.go
@@ -1,18 +1,12 @@
 package weather
 
+import (
+	weather "github.com/kenya6565/weather-scraping-line-bot/app/models/weather"
+)
+
 // WeatherProcessor defines the methods required for processing weather information for cities.
 type WeatherProcessor interface {
-	FetchDataFromJMA() ([]WeatherInfo, error)
-	FilterAreas([]WeatherInfo) ([]AreaInfo, []TimeSeriesInfo)
-}
-
-
-// WeatherData represents raw weather data for a city.
-type WeatherData struct {
-	// Define fields for raw weather data here.
-}
-
-// ProcessedWeatherInfo represents processed weather information for a city.
-type ProcessedWeatherInfo struct {
-	// Define fields for processed weather information here.
+	FetchDataFromJMA() ([]weather.WeatherInfo, error)
+	FilterAreas([]weather.WeatherInfo) ([]weather.AreaInfoInterface, []weather.TimeSeriesInfo)
+	ProcessAreaInfos([]weather.AreaInfoInterface, []weather.TimeSeriesInfo) []string
 }
