62603454a24dd23c6d07ecb904d3b03e4c5bee38
kenya6565 <ken.australia0605@gmail.com>
2023-10-17 23:51:46 +0900

    fix definition of interface

--- app/services/weather/interface.go
+++ app/services/weather/interface.go
@@ -1,12 +1,12 @@
 package weather
 
 import (
-	weather "github.com/kenya6565/weather-scraping-line-bot/app/models/weather"
+	model "github.com/kenya6565/weather-scraping-line-bot/app/model"
 )
 
 // WeatherProcessor defines the methods required for processing weather information for cities.
 type WeatherProcessor interface {
-	FetchDataFromJMA() ([]weather.WeatherInfo, error)
-	FilterAreas([]weather.WeatherInfo) ([]weather.AreaInfo, []weather.TimeSeriesInfo)
-	ProcessAreaInfos([]weather.AreaInfo, []weather.TimeSeriesInfo) []string
+	FetchDataFromJMA() ([]model.WeatherInfo, error)
+	FilterAreas([]model.WeatherInfo) ([]model.AreaInfo, []model.TimeSeriesInfo)
+	ProcessAreaInfos([]model.AreaInfo, []model.TimeSeriesInfo) []string
 }
