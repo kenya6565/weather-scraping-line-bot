d58b2e44e16e45b5c5c5e3cae00c347813b40624
kenya6565 <ken.australia0605@gmail.com>
2023-10-26 23:40:09 +0900

    move process.go to app/services/weather

---
+++ app/services/weather/process.go
@@ -0,0 +1,66 @@
+package weather
+
+import (
+	model "github.com/kenya6565/weather-scraping-line-bot/app/model"
+)
+
+//  ### HERE IS JSON RESPONSE FROM API
+// {
+// 	"timeSeries": [
+// 		# 1
+// 		{
+// 			"timeDefines": [
+// 				"2023-07-27T18:00:00+09:00",
+// 				"2023-07-28T00:00:00+09:00",
+// 				"2023-07-28T06:00:00+09:00",
+// 				"2023-07-28T12:00:00+09:00",
+// 				"2023-07-28T18:00:00+09:00"
+// 			],
+// 			# 2
+// 			"areas": [
+// 				{
+// 					# 3
+// 					"area": {
+// 						"name": "西部",
+// 						# 4
+// 						"code": "140020"
+// 					},
+// 					# 3
+// 					"pops": [
+// 						"30",
+// 						"10",
+// 						"10",
+// 						"20",
+// 						"20"
+// 					]
+// 				}
+// 			]
+// 		}
+// 	]
+// }
+
+func (y *CityWeatherConfig) FilterAreas(weatherReport []model.WeatherInfo) ([]model.AreaInfo, []model.TimeSeriesInfo) {
+	var areas []model.AreaInfo
+	var timeSeriesInfos []model.TimeSeriesInfo
+
+	for _, info := range weatherReport {
+		for _, timeSeries := range info.TimeSeries {
+			for _, area := range timeSeries.Areas {
+				if area.Area.Code == y.AreaCode && area.Pops != nil {
+					areas = append(areas, area)
+					timeSeriesInfos = append(timeSeriesInfos, timeSeries)
+				}
+			}
+		}
+	}
+
+	return areas, timeSeriesInfos
+}
+
+func (y *CityWeatherConfig) ProcessAreaInfos(areas []model.AreaInfo, timeSeriesInfos []model.TimeSeriesInfo) []string {
+	var messages []string
+	for i, area := range areas {
+		messages = append(messages, GeneratePrecipProbMessage(area, timeSeriesInfos[i])...)
+	}
+	return messages
+}
