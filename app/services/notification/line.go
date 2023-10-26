6e71353e57e236c583ab2869c7cbc4204673c9ed
kenya6565 <ken.australia0605@gmail.com>
2023-10-27 02:04:09 +0900

    output it in server log when user sending message

--- app/services/notification/line.go
+++ app/services/notification/line.go
@@ -21,23 +21,61 @@ func HandleEvent(event *linebot.Event) {
 			log.Println("Failed to save user ID to Firestore:", err)
 		}
 
-		// Send a welcome message to the user.
-		welcomeMessage := "Thank you for following our bot! We will provide you with weather updates."
-		if _, err := config.Bot.PushMessage(event.Source.UserID, linebot.NewTextMessage(welcomeMessage)).Do(); err != nil {
-			log.Println("Failed to send welcome message:", err)
-		}
+		// Send a weather update to the user based on their location.
+		// NotifyWeatherToUser(event.Source.UserID, city)
 
-		// Send a weather update to the user.
-		NotifyWeatherToUser(event.Source.UserID)
+	case linebot.EventTypeMessage:
+		if message, ok := event.Message.(*linebot.TextMessage); ok {
+			log.Printf("Received message from user %s: %s", event.Source.UserID, message.Text)
+		}
 	}
 }
+
+// // このサンプルでは、緯度と経度をハードコーディングしている都市のリストを使用します。
+// // 実際の実装では、都市のデータベースやAPIからこの情報を取得することができます。
+// var cities = []CityLocation{
+// 	{Name: "yokohama", Latitude: 35.4437, Longitude: 139.6380},
+// 	// TODO: 他の都市の緯度と経度を追加
+// }
+
+// // determineNearestCity determines the nearest city based on given latitude and longitude.
+// func determineNearestCity(lat, long float64) (string, error) {
+// 	var nearestCity string
+// 	smallestDistance := math.MaxFloat64 // 初期値として非常に大きな値を設定
+
+// 	for _, city := range cities {
+// 		distance := calculateDistance(lat, long, city.Latitude, city.Longitude)
+// 		if distance < smallestDistance {
+// 			smallestDistance = distance
+// 			nearestCity = city.Name
+// 		}
+// 	}
+
+// 	if nearestCity == "" {
+// 		return "", fmt.Errorf("couldn't determine the nearest city for the given location")
+// 	}
+
+// 	return nearestCity, nil
+// }
+
+// // calculateDistance calculates the distance between two geographic coordinates using the Haversine formula.
+// func calculateDistance(lat1, lon1, lat2, lon2 float64) float64 {
+// 	const R = 6371.0 // Earth's radius in kilometers
+
+// 	dLat := (lat2 - lat1) * (math.Pi / 180.0)
+// 	dLon := (lon2 - lon1) * (math.Pi / 180.0)
+
+// 	a := math.Sin(dLat/2)*math.Sin(dLat/2) + math.Cos(lat1*(math.Pi/180.0))*math.Cos(lat2*(math.Pi/180.0))*math.Sin(dLon/2)*math.Sin(dLon/2)
+// 	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+
+// 	distance := R * c
+// 	return distance
+// }
 
 // NotifyWeatherToUser sends a weather report to a specified user.
 // It fetches the latest weather report, processes it to generate a user-friendly message,
 // and then sends this message to the provided user ID.
-func NotifyWeatherToUser(userId string) {
-	city := "yokohama"
-
+func NotifyWeatherToUser(userId, city string) {
 	processor, err := weather.GetWeatherProcessorForCity(city)
 	if err != nil {
 		log.Println("Failed to get weather processor for city:", err)
