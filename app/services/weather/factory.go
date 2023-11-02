5c6c4e2b20473c9ded9888b19b8fd938e36fbe59
kenya6565 <ken.australia0605@gmail.com>
2023-11-03 02:00:05 +0900

    add Tokyo info

--- app/services/weather/factory.go
+++ app/services/weather/factory.go
@@ -12,11 +12,17 @@ type CityWeatherConfig struct {
 func GetWeatherProcessorForCity(city string) (WeatherProcessor, error) {
 	switch city {
 	case "横浜":
+		// CityWeatherConfigはWeatherProcessorインターフェースを実装している
 		return &CityWeatherConfig{
 			JmaApiEndpoint: "https://www.jma.go.jp/bosai/forecast/data/forecast/140000.json",
 			AreaCode:       "140020",
 		}, nil
-	// TODO: 他の都市も必要に応じて追加する
+	case "東京":
+		return &CityWeatherConfig{
+			JmaApiEndpoint: "https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json",
+			AreaCode:       "130010",
+		}, nil
+		// TODO: 他の都市も必要に応じて追加する
 	default:
 		return nil, errors.New("unknown city")
 	}
