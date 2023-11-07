ee5e2c6ecfc6b20e9fbfd3da14ee613e988e8d5f
kenya6565 <ken.australia0605@gmail.com>
2023-11-07 23:53:39 +0900

    remove unused process

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -2,16 +2,6 @@ package main
 
 import "github.com/kenya6565/weather-scraping-line-bot/app/server"
 
-// server "weather-scraping-line-bot/server"
-
 func main() {
-	// weatherReport, err := weather.FetchWeatherReport()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-
-	// areas, timeSeriesInfos := weather.FilterAreas(weatherReport, line.YOKOHAMAWESTAREACODE)
-	// weather.ProcessAreaInfos(areas, timeSeriesInfos)
 	server.StartServer()
 }
