79d2c631cc3b49ae8f31e2d5e2267735f53bf2ce
kenya6565 <ken.australia0605@gmail.com>
2023-11-14 00:45:12 +0900

    add areaName field to CityWeatherConfig

--- app/services/weather/factory.go
+++ app/services/weather/factory.go
@@ -7,6 +7,7 @@ import (
 type CityWeatherConfig struct {
 	JmaApiEndpoint string
 	AreaCode       string
+	AreaName       string
 }
 
 func GetWeatherProcessorForCity(city string) (WeatherProcessor, error) {
@@ -16,16 +17,19 @@ func GetWeatherProcessorForCity(city string) (WeatherProcessor, error) {
 		return &CityWeatherConfig{
 			JmaApiEndpoint: "https://www.jma.go.jp/bosai/forecast/data/forecast/140000.json",
 			AreaCode:       "140020",
+			AreaName:       "西部",
 		}, nil
 	case "東京":
 		return &CityWeatherConfig{
 			JmaApiEndpoint: "https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json",
 			AreaCode:       "130010",
+			AreaName:       "東京地方",
 		}, nil
 	case "大阪":
 		return &CityWeatherConfig{
 			JmaApiEndpoint: "https://www.jma.go.jp/bosai/forecast/data/forecast/270000.json",
 			AreaCode:       "270000",
+			AreaName:       "大阪府",
 		}, nil
 		// TODO: 他の都市も必要に応じて追加
 	default:
