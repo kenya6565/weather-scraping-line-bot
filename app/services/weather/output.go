c8299ba57d9fd90ef3dd0753c2bebd9db72d5431
kenya6565 <ken.australia0605@gmail.com>
2023-11-24 23:40:45 +0900

    refactor output.go

--- app/services/weather/output.go
+++ app/services/weather/output.go
@@ -9,47 +9,37 @@ import (
 )
 
 // GenerateRainMessages generates messages based on precipitation probabilities.
-func GenerateRainMessages(areas []model.AreaInfo, timeSeriesInfos []model.TimeSeriesInfo) []string {
+func GenerateRainMessages(timeSeriesInfos []model.TimeSeriesInfo) []string {
 	var messages []string
-	for i, area := range areas {
-		if len(*area.Pops) < 2 || len(timeSeriesInfos[i].TimeDefines) < 2 {
-			continue
-		}
 
-		for j, popStr := range (*area.Pops)[1:] {
+	for _, series := range timeSeriesInfos {
+		for i, popStr := range series.Areas[0].Pops {
 			pop, err := strconv.Atoi(popStr)
+			// if err != nil || pop <= 0 {
+			// 	continue // Skip if conversion fails or pop is below 20
+			// }
+
+			startTime, endTime, err := getTimeRange(series.TimeDefines[i])
 			if err != nil {
-				fmt.Printf("Error converting pop to integer: %v\n", err)
-				continue
+				continue // Skip if error in getting time range
 			}
-			if pop >= 20 {
-				timeDefine := timeSeriesInfos[i].TimeDefines[j+1]
-				startTime, endTime, err := getTimeRange(timeDefine)
-				if err != nil {
-					fmt.Printf("Error getting time range: %v\n", err)
-					continue
-				}
-				message := fmt.Sprintf("時間: %s ~ %s, 降水確率: %d%%", startTime, endTime, pop)
-				messages = append(messages, message)
-			}
+
+			message := fmt.Sprintf("時間: %s ~ %s, 降水確率: %d%%", startTime, endTime, pop)
+			messages = append(messages, message)
 		}
 	}
+
 	return messages
 }
 
+// APIから取得した時間表記を日本時間に修正する
 func getTimeRange(timeDefine string) (string, string, error) {
 	parsedTime, err := time.Parse(time.RFC3339, timeDefine)
 	if err != nil {
 		return "", "", err
 	}
 	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
-	startTime := parsedTime.In(jst)
-	var endTime time.Time
-	switch startTime.Hour() {
-	case 0, 6, 12, 18:
-		endTime = startTime.Add(time.Hour*5 + time.Minute*59)
-	default:
-		return "", "", fmt.Errorf("unexpected hour: %d", startTime.Hour())
-	}
-	return startTime.Format("2006-01-02 15:04"), endTime.Format("15:04"), nil
+	startTime := parsedTime.In(jst).Format("2006-01-02 15:04")
+	endTime := parsedTime.In(jst).Add(5*time.Hour + 59*time.Minute).Format("15:04")
+	return startTime, endTime, nil
 }
