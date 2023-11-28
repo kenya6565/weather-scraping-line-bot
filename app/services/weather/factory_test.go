a0bf0b07e28d9332488fd4afe59e30a41a9ac471
kenya6565 <ken.australia0605@gmail.com>
2023-11-28 23:45:20 +0900

    create factory_test.go

---
+++ app/services/weather/factory_test.go
@@ -0,0 +1,31 @@
+package weather
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetWeatherProcessorForCity(t *testing.T) {
+	// テストケース
+	testCases := []struct {
+		city           string
+		expectedError  bool
+		expectedConfig *CityWeatherConfig
+	}{
+		{"横浜", false, &CityWeatherConfig{"https://www.jma.go.jp/bosai/forecast/data/forecast/140000.json", "140020", "西部"}},
+		{"東京", false, &CityWeatherConfig{"https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json", "130010", "東京地方"}},
+		{"大阪", false, &CityWeatherConfig{"https://www.jma.go.jp/bosai/forecast/data/forecast/270000.json", "270000", "大阪府"}},
+		{"未知の都市", true, nil},
+	}
+
+	for _, tc := range testCases {
+		processor, err := GetWeatherProcessorForCity(tc.city)
+		if tc.expectedError {
+			assert.Error(t, err, "未知の都市に対してエラーを返すべき")
+		} else {
+			assert.NoError(t, err, "エラーを返すべきではない")
+			assert.Equal(t, tc.expectedConfig, processor, "返された設定が期待値と一致するべき")
+		}
+	}
+}
