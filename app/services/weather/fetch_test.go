cc691ed524733d74f25d7fcce928d307f2f8e8ac
kenya6565 <ken.australia0605@gmail.com>
2023-12-01 01:03:26 +0900

    increase test case fetch_test.go

--- app/services/weather/fetch_test.go
+++ app/services/weather/fetch_test.go
@@ -10,8 +10,11 @@ import (
 )
 
 func TestFetchDataFromJMA(t *testing.T) {
-	// テストサーバーを作成
+	// 気象庁のAPIと同じレスポンスを返すテストサーバーを準備
+	// 事前にAPIのレスポンスを準備しておく方法もあるが、それだとAPIからデータを取得するプロセスをテストすることができない
+	// 今回はAPIを叩いて適切にfetchできているか自体のテストを行うのでテストサーバーを用意すると完全にテストできる
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 指定したJSON文字列をHTTPレスポンスとして書き出す
 		fmt.Fprintln(w, `[{"timeSeries": [{"timeDefines": ["2023-07-27T18:00:00+09:00"], "areas": [{"area": {"name": "西部", "code": "140020"}, "pops": ["30"]}] }] }]`)
 	}))
 	defer ts.Close()
@@ -27,8 +30,22 @@ func TestFetchDataFromJMA(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, weatherReport)
 
-	// エラーケース
+	// エラーケース1: 無効なURL
 	c.JmaApiEndpoint = "invalid_url"
+	_, err = c.FetchDataFromJMA()
+	assert.Error(t, err)
+
+	// エラーケース2: レスポンスボディが読み取れない
+	ts.Close() // テストサーバーを閉じてレスポンスボディが読み取れない状態を作る
+	_, err = c.FetchDataFromJMA()
+	assert.Error(t, err)
+
+	// エラーケース3: JSONのアンマーシャルができない
+	ts = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, `invalid_json`) // 無効なJSONをレスポンスとして返す
+	}))
+	defer ts.Close()
+	c.JmaApiEndpoint = ts.URL
 	_, err = c.FetchDataFromJMA()
 	assert.Error(t, err)
 }
