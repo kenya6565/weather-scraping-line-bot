29fea593ea1918ad127516bc1a6a6f85d92c5293
kenya6565 <ken.australia0605@gmail.com>
2023-12-02 23:05:40 +0900

    delete variables.tf

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "gcp_project_id" {
-  description = "Google Cloud Project ID"
-  type        = string
-}
-
-variable "service_account_key_path" {
-  description = "Path to the GCP service account key JSON file"
-  type        = string
-}
