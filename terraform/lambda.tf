0412b7e9cdc347158985f82cc25bd15ea02e9473
kenya6565 <ken.australia0605@gmail.com>
2023-12-13 00:24:56 +0900

    fix lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,26 +1,28 @@
+resource "null_resource" "build_lambda" {
+  triggers = {
+    build_trigger = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "cd ../app/cmd && GOOS=linux go build -o bootstrap && zip -j bootstrap.zip bootstrap"
+  }
+}
+
 resource "aws_lambda_function" "weather_lambda" {
   function_name = "WeatherLambda"
   runtime       = "provided.al2"
   handler       = "bootstrap" # Lambda ハンドラ関数を指定
 
   # Lambda 関数のソースコードをZIPファイルとして指定
-  filename         = "path/to/your/bootstrap.zip"
-  source_code_hash = filebase64sha256("path/to/your/bootstrap.zip")
+  filename         = "../app/cmd/bootstrap.zip"
+  source_code_hash = filebase64sha256("../app/cmd/bootstrap.zip")
 
   # Lambda 関数に適切なロールを割り当て
   role = aws_iam_role.lambda_role.arn
+
+  depends_on = [null_resource.build_lambda]
 }
 
-resource "aws_lambda_function" "line_webhook_lambda" {
-  function_name = "LineWebhookLambda"
-  runtime       = "provided.al2"
-  handler       = "bootstrap" # Lambda ハンドラ関数を指定
-
-  filename         = "path/to/your/bootstrap.zip"
-  source_code_hash = filebase64sha256("path/to/your/bootstrap.zip")
-
-  role = aws_iam_role.lambda_role.arn
-}
 
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_role"
