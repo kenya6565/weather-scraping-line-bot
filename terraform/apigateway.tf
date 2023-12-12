913c38e9d787a62e59c3f2ce6571f6425b59282a
kenya6565 <ken.australia0605@gmail.com>
2023-12-13 00:01:25 +0900

    create apigateway.tf

---
+++ terraform/apigateway.tf
@@ -0,0 +1,17 @@
+resource "aws_api_gateway_rest_api" "line_webhook_api" {
+  name        = "LineWebhookAPI"
+  description = "API for LINE webhook"
+}
+
+resource "aws_api_gateway_resource" "line_webhook_resource" {
+  rest_api_id = aws_api_gateway_rest_api.line_webhook_api.id
+  parent_id   = aws_api_gateway_rest_api.line_webhook_api.root_resource_id
+  path_part   = "webhook"
+}
+
+resource "aws_api_gateway_method" "line_webhook_method" {
+  rest_api_id   = aws_api_gateway_rest_api.line_webhook_api.id
+  resource_id   = aws_api_gateway_resource.line_webhook_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
