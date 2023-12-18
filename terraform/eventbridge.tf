645849cef1838ffbfcf5ab5b5fcf5e640349ecb1
kenya6565 <ken.australia0605@gmail.com>
2023-12-18 23:42:24 +0900

    add comment out

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -4,11 +4,13 @@ resource "aws_cloudwatch_event_rule" "daily_trigger" {
   schedule_expression = "cron(0 15 * * ? *)" # UTCで15時 (JSTで24時)
 }
 
+# 対象のlambda関数を指定
 resource "aws_cloudwatch_event_target" "trigger_weather_lambda" {
   rule = aws_cloudwatch_event_rule.daily_trigger.name
   arn  = aws_lambda_function.weather_lambda.arn
 }
 
+# EventBridgeからLambda関数を呼び出すためのIAM権限を設定
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_weather_lambda" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
