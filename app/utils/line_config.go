4e6d13c3bd8f7fa330196d7d787ac85c76ad4250
kenya6565 <ken.australia0605@gmail.com>
2023-12-21 01:52:18 +0900

    switch environment variables, depending on local or prd

--- app/utils/line_config.go
+++ app/utils/line_config.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/joho/godotenv"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
@@ -16,18 +19,69 @@ func init() {
 }
 
 func InitLineBot() {
-	// Load environment variables from .env file.
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
+	var err error
+	if os.Getenv("AWS_EXECUTION_ENV") == "AWS_Lambda" {
+		// AWS Lambdaの場合、Parameter Storeから環境変数を読み込む
+		sess := session.Must(session.NewSessionWithOptions(session.Options{
+			SharedConfigState: session.SharedConfigEnable,
+		}))
+		svc := ssm.New(sess, aws.NewConfig().WithRegion("ap-northeast-1"))
 
-	// Initialize the LINE Bot client using credentials from environment variables.
-	Bot, err = linebot.New(
-		os.Getenv("LINE_CHANNEL_SECRET"),
-		os.Getenv("LINE_ACCESS_TOKEN"),
-	)
-	if err != nil {
-		log.Fatalf("Failed to create LINE Bot client: %v", err)
+		// LINE_CHANNEL_SECRETの読み込み
+		secretName := "/app/line_channel_secret"
+		param, err := svc.GetParameter(&ssm.GetParameterInput{
+			Name:           aws.String(secretName),
+			WithDecryption: aws.Bool(true),
+		})
+		if err != nil {
+			log.Fatalf("Failed to fetch parameter %s: %v", secretName, err)
+		}
+		lineChannelSecret := *param.Parameter.Value
+
+		// LINE_ACCESS_TOKENの読み込み
+		secretName = "/app/line_access_token"
+		param, err = svc.GetParameter(&ssm.GetParameterInput{
+			Name:           aws.String(secretName),
+			WithDecryption: aws.Bool(true),
+		})
+		if err != nil {
+			log.Fatalf("Failed to fetch parameter %s: %v", secretName, err)
+		}
+		lineAccessToken := *param.Parameter.Value
+
+		// FIREBASE_PROJECT_IDの読み込み
+		// secretName = "/app/firebase_project_id"
+		// param, err = svc.GetParameter(&ssm.GetParameterInput{
+		// 	Name:           aws.String(secretName),
+		// 	WithDecryption: aws.Bool(true),
+		// })
+		// if err != nil {
+		// 	log.Fatalf("Failed to fetch parameter %s: %v", secretName, err)
+		// }
+		// firebaseProjectId := *param.Parameter.Value
+
+		// 本番環境の場合、parameter storeから環境変数を読み込む
+		Bot, err = linebot.New(
+			lineChannelSecret,
+			lineAccessToken,
+		)
+		if err != nil {
+			log.Fatalf("Failed to create LINE Bot client: %v", err)
+		}
+	} else {
+		// ローカル環境の場合、.envファイルから環境変数を読み込む
+		err = godotenv.Load()
+		if err != nil {
+			log.Fatalf("Error loading .env file")
+		}
+
+		// Initialize the LINE Bot client using credentials from environment variables.
+		Bot, err = linebot.New(
+			os.Getenv("LINE_CHANNEL_SECRET"),
+			os.Getenv("LINE_ACCESS_TOKEN"),
+		)
+		if err != nil {
+			log.Fatalf("Failed to create LINE Bot client: %v", err)
+		}
 	}
 }
