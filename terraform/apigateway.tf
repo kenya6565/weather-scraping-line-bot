f0fe916c1ca988a0e22bc25271d8f4b2f80cb052
kenya6565 <ken.australia0605@gmail.com>
2023-12-23 02:37:27 +0900

    add comment out

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -9,6 +9,7 @@ resource "aws_api_gateway_resource" "line_webhook_resource" {
   path_part   = "webhook"
 }
 
+# /webhookエンドポイントにPOSTリクエストが送られると、設定したLambda関数が実行
 resource "aws_api_gateway_method" "line_webhook_method" {
   rest_api_id   = aws_api_gateway_rest_api.line_webhook_api.id
   resource_id   = aws_api_gateway_resource.line_webhook_resource.id
