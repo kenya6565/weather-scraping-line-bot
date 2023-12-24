ebece827b6222328b06926cbce493c2ffbd023df
kenya6565 <ken.australia0605@gmail.com>
2023-12-24 23:31:49 +0900

    load env variables for firestore

--- app/db/firestore.go
+++ app/db/firestore.go
@@ -6,23 +6,55 @@ import (
 	"os"
 
 	"cloud.google.com/go/firestore"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
 )
 
 // StoreUserID saves a given user ID to the Firestore.
 // This function initializes a Firestore client, and stores the user ID
 // into a "users" collection. If the operation is successful, it returns nil.
 func StoreUserID(userID string) error {
-	// Load environment variables from .env file.
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
+	var FirebaseProjectID, googleApplicationCredentials string
+	var err error
 
-	// Get FirebaseProjectID from the environment variables.
-	FirebaseProjectID := os.Getenv("FIREBASE_PROJECT_ID")
-	if FirebaseProjectID == "" {
-		log.Fatal("FIREBASE_PROJECT_ID must be set in the environment")
+	if os.Getenv("AWS_EXECUTION_ENV") == "AWS_Lambda" {
+		// AWS Lambdaの場合、Parameter Storeから環境変数を読み込む
+		sess := session.Must(session.NewSessionWithOptions(session.Options{
+			SharedConfigState: session.SharedConfigEnable,
+		}))
+		svc := ssm.New(sess, aws.NewConfig().WithRegion("ap-northeast-1"))
+
+		// FIREBASE_PROJECT_IDの読み込み
+		secretName := "/app/firebase_project_id"
+		param, err := svc.GetParameter(&ssm.GetParameterInput{
+			Name:           aws.String(secretName),
+			WithDecryption: aws.Bool(true),
+		})
+		if err != nil {
+			log.Fatalf("Failed to fetch parameter %s: %v", secretName, err)
+		}
+		FirebaseProjectID = *param.Parameter.Value
+
+		// GOOGLE_APPLICATION_CREDENTIALSの読み込み
+		secretName = "/app/google_application_credentials"
+		param, err = svc.GetParameter(&ssm.GetParameterInput{
+			Name:           aws.String(secretName),
+			WithDecryption: aws.Bool(true),
+		})
+		if err != nil {
+			log.Fatalf("Failed to fetch parameter %s: %v", secretName, err)
+		}
+		googleApplicationCredentials = *param.Parameter.Value
+	} else {
+		// ローカル環境の場合、.envファイルから環境変数を読み込む
+		err = godotenv.Load()
+		if err != nil {
+			log.Fatalf("Error loading .env file")
+		}
+		FirebaseProjectID = os.Getenv("FIREBASE_PROJECT_ID")
 	}
 
 	// Create a new context for Firestore operations.
@@ -30,7 +62,8 @@ func StoreUserID(userID string) error {
 
 	// Initialize a Firestore client.
 	// ここでGOOGLE_APPLICATION_CREDENTIALSを読み取っている
-	client, err := firestore.NewClient(ctx, FirebaseProjectID)
+	opt := option.WithCredentialsJSON([]byte(googleApplicationCredentials))
+	client, err := firestore.NewClient(ctx, FirebaseProjectID, opt)
 	if err != nil {
 		return err
 	}
