c6ded8dc08b5c4e87db8d91ed13209333d98c894
kenya6565 <ken.australia0605@gmail.com>
2023-12-26 01:13:54 +0900

    fix parameter_store.tf for google application credentials

--- terraform/parameter_store.tf
+++ terraform/parameter_store.tf
@@ -19,5 +19,17 @@ resource "aws_ssm_parameter" "firebase_project_id" {
 resource "aws_ssm_parameter" "google_application_credentials" {
   name  = "/app/google_application_credentials"
   type  = "SecureString"
-  value = var.google_application_credentials
+  value = jsonencode({
+    type = var.type,
+    project_id = var.project_id,
+    private_key_id = var.private_key_id,
+    private_key = var.private_key,
+    client_email = var.client_email,
+    client_id = var.client_id,
+    auth_uri = var.auth_uri,
+    token_uri = var.token_uri,
+    auth_provider_x509_cert_url = var.auth_provider_x509_cert_url,
+    client_x509_cert_url = var.client_x509_cert_url,
+    universe_domain = var.universe_domain
+  })
 }
