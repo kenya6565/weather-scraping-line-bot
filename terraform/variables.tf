93bf1ea54f26329bef4e961d9f7c7b31c3ccb995
kenya6565 <ken.australia0605@gmail.com>
2023-12-25 22:55:09 +0900

    fix variables.tf for google application credentials

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,14 @@
 variable "line_access_token" {}
 variable "line_channel_secret" {}
 variable "firebase_project_id" {}
-variable "google_application_credentials" {}
+variable "type" {}
+variable "project_id" {}
+variable "private_key_id" {}
+variable "private_key" {}
+variable "client_email" {}
+variable "client_id" {}
+variable "auth_uri" {}
+variable "token_uri" {}
+variable "auth_provider_x509_cert_url" {}
+variable "client_x509_cert_url" {}
+variable "universe_domain" {}
