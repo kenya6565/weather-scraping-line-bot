bbe5fead100df0f18bf96e9806fac1d194e27c3f
kenya6565 <ken.australia0605@gmail.com>
2023-12-30 23:04:54 +0900

    fix line.go to be able to store city info in firestore

--- app/services/notification/line.go
+++ app/services/notification/line.go
@@ -34,7 +34,8 @@ func HandleEvent(event *linebot.Event) {
 
 // handleFollowEvent stores the user's ID to Firestore when the user starts following the bot.
 func handleFollowEvent(event *linebot.Event) {
-	err := db.StoreUserID(event.Source.UserID)
+	ctx := config.CreateContext()
+	err := db.StoreUserID(ctx, event.Source.UserID)
 	if err != nil {
 		log.Printf("Failed to save user ID %s to Firestore: %v", event.Source.UserID, err)
 	}
@@ -45,18 +46,37 @@ func handleFollowEvent(event *linebot.Event) {
 func handleMessageEvent(event *linebot.Event) {
 	if message, ok := event.Message.(*linebot.TextMessage); ok {
 		log.Printf("Received message from user %s: %s", event.Source.UserID, message.Text)
-		NotifyWeatherToUser(event.Source.UserID, message.Text)
+
+		processor, err := weather.GetWeatherProcessorForCity(message.Text)
+		if err != nil {
+			sendMessageToUser(event.Source.UserID, "申し訳ございませんが、その都市の天気情報はサポートされていません。他の都市名を入力してください。")
+			return
+		}
+		// TODO: ここにDBに都市情報保存するメソッド追加
+		// config, ok := processor.(*weather.CityWeatherConfig)
+		// if !ok {
+		// 	log.Println("Failed to assert type")
+		// 	return
+		// }
+		// ctx := db.CreateContext()
+		// // TODO: ここの部分でうまくいっていないので修正する
+		// _, _, err = db.Client.Collection("users").Add(ctx, map[string]interface{}{
+		// 	"userId":         event.Source.UserID,
+		// 	"JmaApiEndpoint": config.JmaApiEndpoint,
+		// 	"AreaCode":       config.AreaCode,
+		// 	"AreaName":       config.AreaName,
+		// })
+		// if err != nil {
+		// 	log.Println("Failed adding user:", err)
+		// 	return
+		// }
+
+		NotifyWeatherToUser(event.Source.UserID, message.Text, processor)
 	}
 }
 
 // NotifyWeatherToUser sends a weather report or an error message to the user.
-func NotifyWeatherToUser(userID, city string) {
-	processor, err := weather.GetWeatherProcessorForCity(city)
-	if err != nil {
-		sendMessageToUser(userID, "申し訳ございませんが、その都市の天気情報はサポートされていません。他の都市名を入力してください。")
-		return
-	}
-
+func NotifyWeatherToUser(userID, city string, processor weather.WeatherProcessor) {
 	weatherReport, err := processor.FetchDataFromJMA()
 	if err != nil {
 		log.Printf("Failed to fetch weather report for city %s: %v", city, err)
