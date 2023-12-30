0e9b06982f402dc49163628aacea2f171a8bfa67
kenya6565 <ken.australia0605@gmail.com>
2023-12-30 22:40:10 +0900

    fix firestore.go to be able to use firestore client in other modules

--- app/db/firestore.go
+++ app/db/firestore.go
@@ -13,10 +13,10 @@ import (
 	"google.golang.org/api/option"
 )
 
-// StoreUserID saves a given user ID to the Firestore.
-// This function initializes a Firestore client, and stores the user ID
-// into a "users" collection. If the operation is successful, it returns nil.
-func StoreUserID(userID string) error {
+var Client *firestore.Client
+
+func InitFirestoreClient() error {
+
 	var FirebaseProjectID, googleApplicationCredentials string
 	var err error
 
@@ -58,7 +58,7 @@ func StoreUserID(userID string) error {
 	}
 
 	// Create a new context for Firestore operations.
-	ctx := context.Background()
+	ctx := CreateContext()
 
 	// Initialize a Firestore client.
 	// ここでGOOGLE_APPLICATION_CREDENTIALSを読み取っている
@@ -70,8 +70,21 @@ func StoreUserID(userID string) error {
 	// Ensure the client is properly closed after operations are complete.
 	defer client.Close()
 
+	Client = client
+	return nil
+}
+
+func CreateContext() context.Context {
+	return context.Background()
+}
+
+// StoreUserID saves a given user ID to the Firestore.
+// This function initializes a Firestore client, and stores the user ID
+// into a "users" collection. If the operation is successful, it returns nil.
+func StoreUserID(ctx context.Context, userID string) error {
+
 	// Add the provided userID to the "users" collection in Firestore.
-	_, _, err = client.Collection("users").Add(ctx, map[string]interface{}{
+	_, _, err := Client.Collection("users").Add(ctx, map[string]interface{}{
 		"userId": userID,
 	})
 	if err != nil {
@@ -83,3 +96,5 @@ func StoreUserID(userID string) error {
 	// Return nil if the user ID is successfully added.
 	return nil
 }
+
+// TODO: ユーザーから送られた都市情報を保存するメソッド書く
