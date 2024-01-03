89009a6ffbb6451f8fa4df36a52b3c527e6a2992
kenya6565 <ken.australia0605@gmail.com>
2024-01-03 20:28:14 +0900

    store city info in firestore

--- app/db/firestore.go
+++ app/db/firestore.go
@@ -19,3 +19,14 @@ func StoreUserID(ctx context.Context, userID string) (*firestore.DocumentRef, er
 	}
 	return docRef, nil
 }
+
+func StoreCityInfo(userID string, details map[string]interface{}) (*firestore.DocumentRef, error) {
+	ctx := config.CreateContext()
+	docRef := config.Client.Collection("users").Doc(userID)
+	_, err := docRef.Set(ctx, details, firestore.MergeAll)
+	if err != nil {
+		log.Println("Failed adding user details:", err)
+		return nil, err
+	}
+	return docRef, nil
+}
