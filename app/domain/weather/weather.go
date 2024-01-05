c08bdbe8f6cc2ad5e6d5111cb45bed51785ca524
kenya6565 <ken.australia0605@gmail.com>
2024-01-05 23:51:33 +0900

    create domain layer

---
+++ app/domain/weather/weather.go
@@ -0,0 +1,16 @@
+package domain
+
+type WeatherInfo struct {
+	TimeSeries []TimeSeriesInfo `json:"timeSeries"`
+}
+type TimeSeriesInfo struct {
+	Areas       []AreaInfo `json:"areas"`
+	TimeDefines []string   `json:"timeDefines"`
+}
+type AreaInfo struct {
+	Area struct {
+		Name string `json:"name"`
+		Code string `json:"code"`
+	} `json:"area"`
+	Pops []string `json:"pops"` // nilを許容しないため、ポインタではなくスライスとして定義
+}
