ac07e925d5bdb45b6f761ba0073bad578d63b47a
kenya6565 <ken.australia0605@gmail.com>
2024-01-08 03:26:10 +0900

    rename presentation notification of presentation layer

--- app/presentation/notifications/handler.go
+++ app/presentation/notifications/handler.go
@@ -1,19 +1,9 @@
-package notification
+package notifications
 
 import (
-	"net/http"
-
 	"github.com/kenya6565/weather-scraping-line-bot/app/services/notification"
-	config "github.com/kenya6565/weather-scraping-line-bot/app/utils"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
-
-func HandleCallback(w http.ResponseWriter, r *http.Request) {
-	events, _ := config.Bot.ParseRequest(r)
-	for _, event := range events {
-		HandleEvent(event)
-	}
-}
 
 // HandleEvent handles incoming Line bot events based on event type.
 // Supported events are 'follow' and 'text message'.
