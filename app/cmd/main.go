eb7f3a493913147f0eddfa19bc81cdba2b7f8f6f
kenya6565 <ken.australia0605@gmail.com>
2024-01-08 22:53:09 +0900

    execute initialization process in main.go

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"os"
 
-	"github.com/kenya6565/weather-scraping-line-bot/app/server"
+	"github.com/kenya6565/weather-scraping-line-bot/app/infrastructure/db"
+	"github.com/kenya6565/weather-scraping-line-bot/app/infrastructure/notification"
+	"github.com/kenya6565/weather-scraping-line-bot/app/infrastructure/server"
 )
 
 func main() {
+	db.InitFirestoreClient()
+	notification.InitLineBot()
+
 	// prd env
 	if os.Getenv("AWS_EXECUTION_ENV") == "AWS_Lambda" {
 		// lambda.Start()
