0f21120fba59f7cc008b640354e7449636aa27c4
kenya6565 <ken.australia0605@gmail.com>
2024-01-09 23:53:29 +0900

    rename line.go to notification.go

--- app/services/notification/notification.go
+++ app/services/notification/notification.go
@@ -1,50 +1,30 @@
-package line
+package notification
 
 import (
 	"log"
-	"net/http"
 	"strings"
 
-	"cloud.google.com/go/firestore"
-	"github.com/kenya6565/weather-scraping-line-bot/app/db"
+	"github.com/kenya6565/weather-scraping-line-bot/app/infrastructure/db"
+	"github.com/kenya6565/weather-scraping-line-bot/app/infrastructure/db/repository"
+	"github.com/kenya6565/weather-scraping-line-bot/app/infrastructure/notification"
 	"github.com/kenya6565/weather-scraping-line-bot/app/services/weather"
-	config "github.com/kenya6565/weather-scraping-line-bot/app/utils"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
-func HandleCallback(w http.ResponseWriter, r *http.Request) {
-	events, _ := config.Bot.ParseRequest(r)
-	for _, event := range events {
-		HandleEvent(event)
-	}
-}
-
-// HandleEvent handles incoming Line bot events based on event type.
-// Supported events are 'follow' and 'text message'.
-func HandleEvent(event *linebot.Event) {
-	switch event.Type {
-	// when user following me
-	case linebot.EventTypeFollow:
-		handleFollowEvent(event)
-
-	// when user sending messages to me
-	case linebot.EventTypeMessage:
-		handleMessageEvent(event)
-	}
-}
-
 // handleFollowEvent stores the user's ID to Firestore when the user starts following the bot.
-func handleFollowEvent(event *linebot.Event) {
-	ctx := config.CreateContext()
-	_, err := db.StoreUserID(ctx, event.Source.UserID)
+func HandleFollowEvent(event *linebot.Event) {
+	ctx := db.CreateContext()
+	_, err := repository.StoreUserID(ctx, event.Source.UserID)
 	if err != nil {
 		log.Printf("Failed to save user ID %s to Firestore: %v", event.Source.UserID, err)
 	}
+	NotifyWeatherToAllUsers()
+
 }
 
 // handleMessageEvent processes the message event.
 // When a user sends a city name, it triggers the weather notification.
-func handleMessageEvent(event *linebot.Event) {
+func HandleMessageEvent(event *linebot.Event) {
 	if message, ok := event.Message.(*linebot.TextMessage); ok {
 		log.Printf("Received message from user %s: %s", event.Source.UserID, message.Text)
 
@@ -53,20 +33,15 @@ func handleMessageEvent(event *linebot.Event) {
 			sendMessageToUser(event.Source.UserID, "申し訳ございませんが、その都市の天気情報はサポートされていません。他の都市名を入力してください。")
 			return
 		}
-		ctx := config.CreateContext()
-		docRef, err := db.StoreUserID(ctx, event.Source.UserID)
-		if err != nil {
-			log.Println("Failed adding user:", err)
-			return
-		}
 
-		_, err = docRef.Set(ctx, map[string]interface{}{
+		cityInfo := map[string]interface{}{
+			"CityName":       processor.GetCityName(),
 			"JmaApiEndpoint": processor.GetJmaApiEndpoint(),
 			"AreaCode":       processor.GetAreaCode(),
 			"AreaName":       processor.GetAreaName(),
-		}, firestore.MergeAll)
-		if err != nil {
-			log.Println("Failed adding user:", err)
+		}
+		if _, err = repository.StoreCityInfo(event.Source.UserID, cityInfo); err != nil {
+			log.Println("Failed adding city info:", err)
 			return
 		}
 
@@ -76,9 +51,6 @@ func handleMessageEvent(event *linebot.Event) {
 
 // NotifyWeatherToUser sends a weather report or an error message to the user.
 func NotifyWeatherToUser(userID, city string, processor weather.WeatherProcessor) {
-
-
-	
 	weatherReport, err := processor.FetchDataFromJMA()
 	if err != nil {
 		log.Printf("Failed to fetch weather report for city %s: %v", city, err)
@@ -98,7 +70,26 @@ func NotifyWeatherToUser(userID, city string, processor weather.WeatherProcessor
 
 // sendMessageToUser sends a text message to the specified user.
 func sendMessageToUser(userID, message string) {
-	if _, err := config.Bot.PushMessage(userID, linebot.NewTextMessage(message)).Do(); err != nil {
+	if _, err := notification.Bot.PushMessage(userID, linebot.NewTextMessage(message)).Do(); err != nil {
 		log.Printf("Failed to send message to user %s: %v", userID, err)
+	}
+}
+
+func NotifyWeatherToAllUsers() {
+	users, err := repository.GetAllUsers()
+	if err != nil {
+		log.Printf("Failed to get all users: %v", err)
+		return
+	}
+
+	for _, user := range users {
+		userID := user["UserId"].(string)
+		city := user["CityName"].(string)
+		processor, err := weather.GetWeatherProcessorForCity(city)
+		if err != nil {
+			log.Printf("Failed to get weather processor for city %s: %v", city, err)
+			continue
+		}
+		NotifyWeatherToUser(userID, city, processor)
 	}
 }
