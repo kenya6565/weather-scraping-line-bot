b07892a2b383ba83ed9d4f3616a29b06b68ad641
kenya6565 <ken.australia0605@gmail.com>
2024-01-10 23:42:40 +0900

    fix firestore.go to gather process of setting up firestore

--- app/infrastructure/db/firestore.go
+++ app/infrastructure/db/firestore.go
@@ -3,47 +3,77 @@ package db
 import (
 	"context"
 	"log"
+	"os"
 
 	"cloud.google.com/go/firestore"
-	config "github.com/kenya6565/weather-scraping-line-bot/app/utils"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
+	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
 )
 
-func StoreUserID(ctx context.Context, userID string) (*firestore.DocumentRef, error) {
-	docRef := config.Client.Collection("users").Doc(userID)
-	_, err := docRef.Set(ctx, map[string]interface{}{
-		"UserId": userID,
-	}, firestore.MergeAll)
-	if err != nil {
-		log.Println("Failed adding user:", err)
-		return nil, err
-	}
-	return docRef, nil
+var Client *firestore.Client
+
+func CreateContext() context.Context {
+	return context.Background()
 }
 
-func StoreCityInfo(userID string, details map[string]interface{}) (*firestore.DocumentRef, error) {
-	ctx := config.CreateContext()
-	docRef := config.Client.Collection("users").Doc(userID)
-	_, err := docRef.Set(ctx, details, firestore.MergeAll)
+func InitFirestoreClient() error {
+
+	var FirebaseProjectID, googleApplicationCredentials string
+	var err error
+
+	if os.Getenv("AWS_EXECUTION_ENV") == "AWS_Lambda" {
+		// AWS Lambdaの場合、Parameter Storeから環境変数を読み込む
+		sess := session.Must(session.NewSessionWithOptions(session.Options{
+			SharedConfigState: session.SharedConfigEnable,
+		}))
+		svc := ssm.New(sess, aws.NewConfig().WithRegion("ap-northeast-1"))
+
+		// FIREBASE_PROJECT_IDの読み込み
+		secretName := "/app/firebase_project_id"
+		param, err := svc.GetParameter(&ssm.GetParameterInput{
+			Name:           aws.String(secretName),
+			WithDecryption: aws.Bool(true),
+		})
+		if err != nil {
+			log.Fatalf("Failed to fetch parameter %s: %v", secretName, err)
+		}
+		FirebaseProjectID = *param.Parameter.Value
+
+		// GOOGLE_APPLICATION_CREDENTIALSの読み込み
+		secretName = "/app/google_application_credentials"
+		param, err = svc.GetParameter(&ssm.GetParameterInput{
+			Name:           aws.String(secretName),
+			WithDecryption: aws.Bool(true),
+		})
+		if err != nil {
+			log.Fatalf("Failed to fetch parameter %s: %v", secretName, err)
+		}
+		googleApplicationCredentials = *param.Parameter.Value
+	} else {
+		// ローカル環境の場合、.envファイルから環境変数を読み込む
+		err = godotenv.Load()
+		if err != nil {
+			log.Fatalf("Error loading .env file")
+		}
+		FirebaseProjectID = os.Getenv("FIREBASE_PROJECT_ID")
+		googleApplicationCredentials = os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+	}
+
+	// Create a new context for Firestore operations.
+	ctx := CreateContext()
+
+	// Initialize a Firestore client.
+	// ここでGOOGLE_APPLICATION_CREDENTIALSを読み取っている
+	opt := option.WithCredentialsJSON([]byte(googleApplicationCredentials))
+	client, err := firestore.NewClient(ctx, FirebaseProjectID, opt)
 	if err != nil {
-		log.Println("Failed adding user details:", err)
-		return nil, err
-	}
-	return docRef, nil
-}
-
-func GetAllUsers() ([]map[string]interface{}, error) {
-	ctx := config.CreateContext()
-	iter := config.Client.Collection("users").Documents(ctx)
-	docs, err := iter.GetAll()
-	if err != nil {
-		log.Printf("Failed to get user documents: %v", err)
-		return nil, err
+		log.Fatalf("Failed to create Firestore client: %v", err)
+		return err
 	}
 
-	var users []map[string]interface{}
-	for _, doc := range docs {
-		users = append(users, doc.Data())
-	}
-
-	return users, nil
+	Client = client
+	return nil
 }
