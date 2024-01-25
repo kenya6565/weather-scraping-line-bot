48be9e8a34ac0994086a6557657f1e1bfb3ff69d
kenya6565 <ken.australia0605@gmail.com>
2024-01-25 23:26:12 +0900

    pass process_test.go

--- app/services/weather/process_test.go
+++ app/services/weather/process_test.go
@@ -3,7 +3,7 @@ package weather
 import (
 	"testing"
 
-	"github.com/kenya6565/weather-scraping-line-bot/app/domain/weather"
+	domain "github.com/kenya6565/weather-scraping-line-bot/app/domain/weather"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -39,13 +39,14 @@ func TestTransformWeatherData(t *testing.T) {
 	}
 
 	// 正常ケース
-	result := cwc.TransformWeatherData(weatherInfo)
+	result, err := cwc.TransformWeatherData(weatherInfo)
+	assert.NoError(t, err, "エラーが発生しないこと")
 	assert.NotEmpty(t, result, "結果は空であってはならない")
 	assert.Equal(t, 4, len(result[0].TimeDefines), "先頭のTimeDefinesが除去されていること")
 	assert.Equal(t, 4, len(result[0].Areas[0].Pops), "先頭のPopsが除去されていること")
 
 	// 異常ケース
 	weatherInfo[0].TimeSeries[0].TimeDefines = weatherInfo[0].TimeSeries[0].TimeDefines[:4] // TimeDefinesの要素を4つに減らす
-	result = cwc.TransformWeatherData(weatherInfo)
-	assert.Empty(t, result, "TimeDefinesの要素が5でない場合、結果は空でなければならない")
+	result, _ = cwc.TransformWeatherData(weatherInfo)
+	assert.Empty(t, result, "結果は空でなければならない")
 }
