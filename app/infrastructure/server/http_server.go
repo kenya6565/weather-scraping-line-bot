fa6e8c85689cb4d26fb7e5302438531d2b396334
kenya6565 <ken.australia0605@gmail.com>
2024-01-29 23:16:37 +0900

    fix: delete ActivateLambda method

--- app/infrastructure/server/http_server.go
+++ app/infrastructure/server/http_server.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/kenya6565/weather-scraping-line-bot/app/infrastructure/notification"
 	"github.com/kenya6565/weather-scraping-line-bot/app/presentation/notifications"
-	n "github.com/kenya6565/weather-scraping-line-bot/app/services/notification"
 )
 
 func HandleCallback(w http.ResponseWriter, r *http.Request) {
@@ -22,9 +21,4 @@ func ActivateLocalServer() {
 	log.Println("Starting server on :8080...")
 	// activate http server
 	http.ListenAndServe(":8080", nil)
-}
-
-// TODO: Lambda環境で必要なネットワーク処理を記載
-func ActivateLambda() {
-	n.NotifyWeatherToAllUsers()
 }
