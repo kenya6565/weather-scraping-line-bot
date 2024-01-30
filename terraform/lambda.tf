59ec016732ba21a8505e7f9305ffe88c23bdf37f
kenya6565 <ken.australia0605@gmail.com>
2024-01-31 02:30:23 +0900

    infra: create new end point for line webhook event

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -81,3 +81,26 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+# Lambda関数をトリガーするエンドポイントを作成
+resource "aws_api_gateway_resource" "line_message_resource" {
+  rest_api_id = aws_api_gateway_rest_api.line_webhook_api.id
+  parent_id   = aws_api_gateway_rest_api.line_webhook_api.root_resource_id
+  path_part   = "message"
+}
+
+resource "aws_api_gateway_method" "line_message_method" {
+  rest_api_id   = aws_api_gateway_rest_api.line_webhook_api.id
+  resource_id   = aws_api_gateway_resource.line_message_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "line_message_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.line_webhook_api.id
+  resource_id             = aws_api_gateway_resource.line_message_resource.id
+  http_method             = aws_api_gateway_method.line_message_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.weather_lambda.invoke_arn
+}
