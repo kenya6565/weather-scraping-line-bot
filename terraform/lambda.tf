5dbf1e0172bea3f3803dd16b203e177dd2145def
kenya6565 <ken.australia0605@gmail.com>
2024-01-31 22:52:16 +0900

    infra: fix api gateway integration with lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -96,10 +96,10 @@ resource "aws_api_gateway_method" "line_message_method" {
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "line_message_integration" {
+resource "aws_api_gateway_integration" "line_webhook_integration" {
   rest_api_id             = aws_api_gateway_rest_api.line_webhook_api.id
-  resource_id             = aws_api_gateway_resource.line_message_resource.id
-  http_method             = aws_api_gateway_method.line_message_method.http_method
+  resource_id             = aws_api_gateway_resource.line_webhook_resource.id
+  http_method             = aws_api_gateway_method.line_webhook_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.weather_lambda.invoke_arn
