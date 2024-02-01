decd3912752e818ff6bbbdacdeff4b32f385ff56
kenya6565 <ken.australia0605@gmail.com>
2024-02-02 00:04:54 +0900

    infra: deploy prd apigateway endpoint

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -9,10 +9,33 @@ resource "aws_api_gateway_resource" "line_webhook_resource" {
   path_part   = "webhook"
 }
 
+resource "aws_api_gateway_integration" "line_webhook_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.line_webhook_api.id
+  resource_id             = aws_api_gateway_resource.line_webhook_resource.id
+  http_method             = aws_api_gateway_method.line_webhook_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.weather_lambda.invoke_arn
+}
+
 # /webhookエンドポイントにPOSTリクエストが送られると、設定したLambda関数が実行
 resource "aws_api_gateway_method" "line_webhook_method" {
   rest_api_id   = aws_api_gateway_rest_api.line_webhook_api.id
   resource_id   = aws_api_gateway_resource.line_webhook_resource.id
   http_method   = "POST"
   authorization = "NONE"
+}
+
+resource "aws_api_gateway_deployment" "prd_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.line_webhook_api.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "prd_stage" {
+  stage_name    = "prd"
+  rest_api_id   = aws_api_gateway_rest_api.line_webhook_api.id
+  deployment_id = aws_api_gateway_deployment.prd_deployment.id
 }
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -95,12 +95,3 @@ resource "aws_api_gateway_method" "line_message_method" {
   http_method   = "POST"
   authorization = "NONE"
 }
-
-resource "aws_api_gateway_integration" "line_webhook_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.line_webhook_api.id
-  resource_id             = aws_api_gateway_resource.line_webhook_resource.id
-  http_method             = aws_api_gateway_method.line_webhook_method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.weather_lambda.invoke_arn
-}
