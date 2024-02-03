463d9c83243541fe531be37f12e4cb1204391524
kenya6565 <ken.australia0605@gmail.com>
2024-02-03 23:45:52 +0900

    refactor: create LineEvent struct

---
+++ app/domain/notification/notification.go
@@ -0,0 +1,15 @@
+package domain
+
+type LineEvent struct {
+	Events []struct {
+		Type       string `json:"type"`
+		ReplyToken string `json:"replyToken"`
+		Source     struct {
+			UserID string `json:"userId"`
+		} `json:"source"`
+		Message struct {
+			Type string `json:"type"`
+			Text string `json:"text"`
+		} `json:"message"`
+	} `json:"events"`
+}
