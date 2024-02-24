7a9edf9dab0e5e8e8e8974022244d6b17c6485e0
kenya6565 <ken.australia0605@gmail.com>
2024-02-24 23:41:01 +0900

    test: add new test cases in process.go

--- app/services/weather/process_test.go
+++ app/services/weather/process_test.go
@@ -49,4 +49,14 @@ func TestTransformWeatherData(t *testing.T) {
 	weatherInfo[0].TimeSeries[0].TimeDefines = weatherInfo[0].TimeSeries[0].TimeDefines[:4] // TimeDefinesの要素を4つに減らす
 	result, _ = cwc.TransformWeatherData(weatherInfo)
 	assert.Empty(t, result, "結果は空でなければならない")
+
+	// 異常ケース: 対応するエリアが見つからない
+	weatherInfo = createSampleWeatherInfo()
+	cwc = CityWeatherConfig{
+		AreaCode: "999999",
+		AreaName: "存在しないエリア",
+	}
+	result, err = cwc.TransformWeatherData(weatherInfo)
+	assert.Error(t, err, "対応するエリアが見つからない場合、エラーが発生する")
+	assert.Nil(t, result, "結果はnilであるべき") // "ni" を "nil" に修正
 }
